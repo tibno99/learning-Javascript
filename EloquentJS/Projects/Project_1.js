00d801236694ec18d95ad9d6ac1690b3f76404b2
Tobin Eberle <teberle@precisiondrilling.com>
2020-07-21 16:25:33 -0600

    project one update

--- EloquentJS/Projects/Project_1.js
+++ EloquentJS/Projects/Project_1.js
@@ -53,5 +53,24 @@ class VillageState{
 }
 
  
- console.log(roadGraph);
+function runRobot(state, robot, memory){
+    for(let turn = 0;;turn++){
+        if(state.parcels.length == 0){
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
 
+
+
+
+ let first = new VillageState("Post Office", [{place: "Post Office", address: "Alice's House"}]);
+
+ let next = first.move("Alice's House");
+
+ console.log(next);
